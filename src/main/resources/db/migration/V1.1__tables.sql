5473438b9b3d1c694f6eb8b210cc546d2e9d087e
Alexandros Lagios <alexandroslagios@gmail.com>
2020-03-20 10:12:12 +0200

    feat: Add doctor, patient and virus import files
    The doctor import file is fixed length, the patient one is colon-separated
    and the virus one is delimited with "|".

--- src/main/resources/db/migration/V1.1__tables.sql
+++ src/main/resources/db/migration/V1.1__tables.sql
@@ -1,11 +1,23 @@
-CREATE TABLE infections (
+create TABLE infections (
     id bigint PRIMARY KEY,
     name varchar(200) NOT NULL, 
     type varchar(200) NOT NULL,
-    clinical_description varchar(500)
+    description varchar(500)
 );
 
-CREATE TABLE patients (
+create sequence infection_id_seq start with 1000 increment by 50;
+
+create TABLE doctors (
+    id bigint PRIMARY KEY, 
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    gender varchar(20) NOT NULL,
+    phone_number varchar(20)
+);
+
+create sequence doctor_id_seq start with 1000 increment by 50;
+
+create TABLE patients (
     id bigint PRIMARY KEY, 
     first_name varchar(50) NOT NULL,
     last_name varchar(50) NOT NULL,
@@ -20,11 +32,6 @@ CREATE TABLE patients (
     doctor_id bigint
 );
 
-CREATE TABLE doctors (
-    id bigint PRIMARY KEY, 
-    first_name varchar(50) NOT NULL,
-    last_name varchar(50) NOT NULL,
-    gender varchar(20) NOT NULL,
-    phone_number varchar(20)
-);
+create sequence patient_id_seq start with 1000 increment by 50;
+
 
